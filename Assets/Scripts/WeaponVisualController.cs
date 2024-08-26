f085e63dc7926cb483bf0a78706448290acc63bc
postall74 <postall_mcs@mail.ru>
2024-08-27 02:07:38 +0500

    WeponVisualController.cs and Gun Settings
    Добавлено оружие в правую руку персонажа. Добавлен скрипт WeaponVisualController.cs для визуального контроля оружия в руках персонажа, и создана первая часть кода, для переключение оружия по клавишам от 1 до 7

---
+++ Assets/Scripts/WeaponVisualController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponVisualController : MonoBehaviour
+{
+    #region Constants
+    #endregion
+
+    #region Fields
+    [SerializeField] private Transform[] _gunTransforms;
+    [SerializeField] private Transform _pistol, _revolver, _rifle, _shotgun_1, _shotgun_2, _smg, _sniperRifle;
+    #endregion
+
+    #region Properties
+    #endregion
+
+    #region Unity Methods
+    private void Start()
+    {
+        SwitchOfGuns();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+            SwitchOn(_pistol);
+
+        if (Input.GetKey(KeyCode.Alpha2))
+            SwitchOn(_revolver);
+        
+        if (Input.GetKey(KeyCode.Alpha3))
+            SwitchOn(_rifle);
+        
+        if (Input.GetKey(KeyCode.Alpha4))
+            SwitchOn(_shotgun_1);
+        
+        if (Input.GetKey(KeyCode.Alpha5))
+            SwitchOn(_shotgun_2);
+        
+        if (Input.GetKey(KeyCode.Alpha6))
+            SwitchOn(_smg);
+        
+        if (Input.GetKey(KeyCode.Alpha7))
+            SwitchOn(_sniperRifle);
+    }
+
+    #endregion
+
+    #region Methods
+    private void SwitchOfGuns()
+    {
+        foreach (Transform gun in _gunTransforms)
+        {
+            gun.gameObject.SetActive(false);
+        }
+    }
+
+    private void SwitchOn(Transform gunTransform)
+    {
+        SwitchOfGuns();
+        gunTransform.gameObject.SetActive(true);
+    }
+    #endregion
+}
+
+
+
